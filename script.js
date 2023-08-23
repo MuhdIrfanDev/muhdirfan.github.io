e03e60be8b146123530b855228086ca85ea1dc5b
MuhdIrfanDev <142289808+MuhdIrfanDev@users.noreply.github.com>
2023-08-23 17:39:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ $(document).ready(function(){
         loop: true
     });
 
-    // var typed = new Typed(".typing-2", {
-    //     strings: ["Developer", "Blogger", "Designer", "Freelancer", ""],
-    //     typeSpeed: 100,
-    //     backSpeed: 60,
-    //     loop: true
-    // });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Blogger", "Designer", "Freelancer", ""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     //owl carousel script
     $('.carousel').owlCarousel({
